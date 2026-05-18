4acd6655687cef84054429ee45415748f1e77638
li66881 <lishuxian100721@gmail.com>
2026-05-19 01:06:05 +0800

    Clean environment files and fix admin route

--- \src\sanity\schemas\index.ts
+++
@@ -1,5 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import artist from './artist'
-import artwork from './artwork'
-
-export const schemaTypes: SchemaTypeDefinition[] = [artist, artwork]
